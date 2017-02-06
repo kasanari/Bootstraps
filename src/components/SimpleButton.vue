42c77eb72e4e25c30988d8138ee38044b25efd7b
Michael Rehn <rehn.michael@outlook.com>
2017-02-06 18:37:19 +0100

    Added example button component

---
+++ src/components/SimpleButton.vue
@@ -0,0 +1,34 @@
+<template>
+    <button @click="increment()">{{ buttonText }} -- {{ counter }}</button>
+
+</template>
+
+<script>
+    export default {
+        name: 'simple-button',
+        data () {
+            return {
+                counter: 0
+            }
+        },
+        props: [
+            'buttonText'
+        ],
+        methods: {
+            increment () {
+                this.counter++;
+            }
+        }
+    }
+</script>
+
+<style scoped>
+button {
+    background-color: rgb(220, 80, 80);
+    padding: 1em;
+    border: none;
+    border-radius: 2px;
+    color: rgba(255, 255, 255, 0.8);
+}
+</style>
+
--- src/index/App.vue
+++ src/index/App.vue
@@ -1,10 +1,21 @@
 <template>
-    <h1>Index osv mm</h1>
+    <div>
+        <h1>Index osv mm</h1>
+        <simple-button buttonText="En knapp"></simple-button>
+        <simple-button buttonText="En annan knapp"></simple-button>
+        <simple-button buttonText="En tredje knapp"></simple-button>
+        <button>En knapp som inte är 'simple-button'</button>
+    </div>
 </template>
 
 <script>
+    import SimpleButton from '../components/SimpleButton.vue'
+
     export default {
-        name: 'app'
+        name: 'app',
+        components: {            
+            SimpleButton
+        }
     }
 </script>
 

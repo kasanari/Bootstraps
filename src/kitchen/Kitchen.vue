09bde780d2a784e3e31545197e3e1ba19a61014a
Michael <rehn.michael@outlook.com>
2017-02-15 21:53:32 +0100

    Added UI component to show kitchen orders

--- src/kitchen/Kitchen.vue
+++ src/kitchen/Kitchen.vue
@@ -2,16 +2,19 @@
     <div>
         <h1>Köket</h1>
         <simple-button buttonText="En knapp"></simple-button>
+        <simple-orders />
     </div>
 </template>
 
 <script>
     import SimpleButton from '../components/SimpleButton.vue'
+    import SimpleOrders from '../components/SimpleOrders.vue'
 
     export default {
         name: 'kitchen',
         components: {            
-            SimpleButton
+            SimpleButton,
+            SimpleOrders
         }
     }
 </script>

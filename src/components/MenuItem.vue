9c345623bda6bae9c82ef9d409fd040ffd5ae659
Michael <rehn.michael@outlook.com>
2017-02-15 16:46:31 +0100

    Styled menu a bit

--- src/components/MenuItem.vue
+++ src/components/MenuItem.vue
@@ -1,9 +1,12 @@
 <template>
-    <div 
-        class="item-container"
-        v-bind:style="itemContainerStyle">
-        <p>{{ item.label }}</p>
-    </div>
+    <div class="item-container">
+        <div 
+            class="item"
+            v-bind:style="itemStyle"
+        >
+            <p class="item-label">{{ item.label }}</p>
+        </div>
+    </div> 
 </template>
 
 <script>
@@ -14,7 +17,7 @@ export default {
     ],
     data() {
         return {
-            itemContainerStyle: {
+            itemStyle: {
                 backgroundImage: 'url(' + this.item.img + ')'
             }
         }
@@ -24,8 +27,34 @@ export default {
 
 <style scoped>
 .item-container {
+    padding: 5px;
+}
+
+.item {
     width: 200px;
+    height: 100%;
     background-position: center;
     background-size: cover;
+
+    position: relative;
+
+    cursor: pointer;
+    box-shadow: 0 0 4px 0 rgba(0, 0, 0, 0.8);
+}
+
+.item:hover {
+    filter: brightness(1.3);
+}
+
+.item-label {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    text-align: center;
+    background: rgba(0, 0, 0, 0.5);
+    color: white;
+    padding: 0.2em;
+    font-size: 1.2rem;
 }
 </style>

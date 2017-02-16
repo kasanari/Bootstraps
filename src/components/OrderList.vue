538610bf07e3cb3a530f28c3f5427f4a9ffe37fa
Michael <rehn.michael@outlook.com>
2017-02-16 14:29:28 +0100

    Added prices and made order list more fancy

--- src/components/OrderList.vue
+++ src/components/OrderList.vue
@@ -1,9 +1,39 @@
 <template>
     <div>
-        <p>Tap items to add orders</p>
-        {{order}}
+        <h1>Order #{{order.getOrderNumber()}}</h1>
+        <h3>Tap items to add orders</h3>
+        <p class="category-label">FOODS</p>
+        <div class="item-container" v-for="food in order.getFoods()">
+            <p class="item-name">{{getFoodLabel(food)}}, {{getFoodPrice(food)}}kr</p>
+            <p class="item-note" v-if="food.getNote() !== ''">
+                {{food.getNote()}}
+            </p>
+        </div>
+        <p class="category-label">DRINKS</p>
+        <div class="item-container" v-for="drink in order.getDrinks()">
+            <p class="item-name">{{getDrinkLabel(drink)}}</p>
+        </div>
     </div>
 </template>
+
+<style scoped>
+.category-label {
+    font-size: 0.8em;
+    color: rgba(200, 200, 200, 1);
+    margin-bottom: 0.4em;
+    margin-top: 0.4em;
+}
+
+.item-name {
+    padding-top: 0.3em;
+    padding-bottom: 0.3em;
+}
+
+.item-container:hover {
+    background-color: rgba(200, 200, 200, 1);
+    cursor: pointer;
+}
+</style>
 
 <script>
 export default {
@@ -17,6 +47,26 @@ export default {
         this.clientAPI.addOrderChangedListener((newOrder) => {
             this.order = newOrder;
         })
+    },
+    methods: {
+        getFoodLabel(food) {
+            let menu = this.clientAPI.getMenu();
+            let foodItem = menu.foods.find((fi) => fi._id === food.getId());
+
+            return foodItem.label;
+        },
+        getDrinkLabel(drink) {
+            let menu = this.clientAPI.getMenu();
+            let drinkItem = menu.drinks.find((di) => di._id === drink.getId());
+
+            return drinkItem.label;
+        },
+        getFoodPrice(food) {
+            let menu = this.clientAPI.getMenu();
+            let foodItem = menu.foods.find((fi) => fi._id === food.getId());
+
+            return foodItem.price;
+        }
     }
 }
 </script>

b668fc16ee6fde6ee32f338431302dce6eea895e
kasanari <jakobnnyberg@gmail.com>
2017-02-19 14:24:46 +0100

    Lade till betalningstypknappar

---
+++ src/components/Payment.vue
@@ -0,0 +1,79 @@
+<!-- #27ae60 -->
+
+<template>
+    <div>
+    <div class="main">
+        <pay-button v-on:switch="setType" v-bind:class="{active: isCash }" class="cash" v-bind:label = '"cash"'></pay-button>
+        <pay-button v-on:switch="setType" v-bind:class="{active: !isCash }" class="card" v-bind:label = '"card"'></pay-button>
+        
+    </div>
+    </div>
+   
+</template>
+
+<script>
+    import PayButton from '../components/PayButton.vue';
+    export default {
+        name: 'payment',
+        data: function () {
+            return {
+                isCash: true,
+                isCard: false
+            }
+        },
+      
+        props: [
+        ],
+        methods: {
+            setType: function (type) {
+                if (type === "cash") {
+                    this.isCash = true;
+                } else {
+                    this.isCash = false;
+                }
+                
+            }
+        },
+         components: {            
+          PayButton
+        }
+    }
+</script>
+
+<style scoped>
+
+    .main {
+        height: 100px;
+        margin: auto;
+        width: 70%;
+        display: flex;
+        flex-direction: row;
+        align-items: stretch;
+    }
+    
+    .active {
+
+        background-color: rgba(39, 174, 96,1.0);
+
+
+    }
+    
+    p {
+        padding: 10px;
+        display: block;
+        width: 50%;
+        border: solid 1px;
+        margin: auto;
+        text-align: center;
+    }
+
+    .card {
+            background-image: url(https://upload.wikimedia.org/wikipedia/commons/a/a7/Emoji_u1f4b3.svg);
+    }
+    
+    .cash {
+            background-image: url(https://upload.wikimedia.org/wikipedia/commons/a/af/Emojione_1F4B6.svg);
+    }
+    
+</style>
+
--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -3,6 +3,7 @@
     <div> 
         <order-list />
         <table-picker ></table-picker>
+        <payment/>
         <button @click="sendOrder">Send to kitchen</button>
     </div>
     
@@ -10,6 +11,7 @@
 </template>
 
 <script>
+    import Payment from '../components/Payment.vue';
     import TablePicker from '../components/TablePicker.vue';
     import OrderList from '../components/OrderList.vue';
   
@@ -31,7 +33,8 @@
         },
         components: {            
           TablePicker,
-          OrderList
+            OrderList,
+            Payment
         }
     }
 </script>
@@ -40,5 +43,16 @@
     div {
         background-color: rgba(236, 240, 241,1.0);
         padding: 20px;
+    }
+    
+    button {
+        display: block;
+        margin: auto;
+        margin-top: 20px;
+        padding: 10px;
+        font-family: inherit;
+        font-size: 150%;
+        font-weight: bold;
+        
     }
 </style>
